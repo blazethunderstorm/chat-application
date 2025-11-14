d785c38b265d09f8ceb5393a2c0526ef5c113bf0
ANIRUDH <anirudhnarang0@gmail.com>
2025-11-14 21:19:26 +0530

    changed user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -20,6 +20,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: "",
     },
+    lastSeen: {
+      type: Date,
+      default: Date.now, // this is added to get the recent time user was online
+    },
   },
   { timestamps: true }
 );
